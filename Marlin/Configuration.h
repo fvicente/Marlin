72bc48f7daaf115657a80f596cd5929d6cd7163e
Fernando Vicente <fvicente@gmail.com>
2015-05-14 12:55:33 -0300

    Adjusted Z axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -518,7 +518,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // default settings
 
 // FGV
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,795.82} // 838
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,3055.5555,795.82} // 838
 #define DEFAULT_MAX_FEEDRATE {500, 500, 1, 25}
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,200.0*8/3,760*1.1}  // default steps per unit for Ultimaker
 //#define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
